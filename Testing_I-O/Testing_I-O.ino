bcffcc6fc0698864bcb782708e0734180d890492
Gabriel Morris <Gabriel.Morris@altec.com>
2021-08-03 08:54:32 -0500

    touched up testing

--- Testing_I-O/Testing_I-O.ino
+++ Testing_I-O/Testing_I-O.ino
@@ -8,7 +8,7 @@ void setup() {
   
   X.attach(22, 23, 24, 25);  // Direction/A is pin 22, Step/B is pin 23, Enable is pin 24, HLFB is pin 25
 
-  X.setMaxVel(20);  // Value between 2 and 100,000 steps/sec
+  X.setMaxVel(20000);  // Value between 2 and 100,000 steps/sec
   X.setMaxAccel(50000);  // Value between 4000 and 2,000,000 steps/sec^2
 
   X.enable();
@@ -16,23 +16,13 @@ void setup() {
   delay(100);
 
   machine.Start();
+
+  Serial.begin(9600);
 }
 
 void loop() {
-
-  X.move(8000);
-
-  while (X.readHLFB()) {
-    
-  }
-
+  X.move(800);
   delay(20);
-
-  X.move(-8000);
-
-  while (X.readHLFB()) {
-    
-  }
-
+  X.move(-800);
   delay(20);
 }
